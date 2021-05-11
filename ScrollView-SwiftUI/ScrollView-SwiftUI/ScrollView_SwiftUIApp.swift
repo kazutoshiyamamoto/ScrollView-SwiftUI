6e4e63d1e915896f378a4b27d633265688f67617
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-05-11 21:17:53 +0900

    初回コミット

---
+++ ScrollView-SwiftUI/ScrollView-SwiftUI/ScrollView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_SwiftUIApp.swift
+//  ScrollView-SwiftUI
+//
+//  Created by home on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
