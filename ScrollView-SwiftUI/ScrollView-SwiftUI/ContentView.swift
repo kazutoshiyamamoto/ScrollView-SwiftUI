ac7551711582e9a3f5b7064ca6a58779b00b346c
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-05-11 22:32:06 +0900

    タップした位置までスクロールするサンプルを追加

--- ScrollView-SwiftUI/ScrollView-SwiftUI/ContentView.swift
+++ ScrollView-SwiftUI/ScrollView-SwiftUI/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var regions = ["北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州", "北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州", "北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(Array(zip(regions.indices, regions)), id: \.0) { index, region in
+                        Text(region)
+                            .padding()
+                            .font(.title3)
+                            .onTapGesture {
+                                withAnimation(.interactiveSpring(response: 0.1)) {
+                                    proxy.scrollTo(index, anchor: .top)
+                                }
+                            }
+                    }
+                    .frame(width: geometry.size.width, alignment: .leading)
+                }
+            }
+        }
     }
 }
 
